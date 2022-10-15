4b3c957d2cb4f85a98a0025ac77ab37f97da3e9e
keyrun584 <89588350+keyrun584@users.noreply.github.com>
2022-10-15 23:41:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,18 +5,15 @@ s1=pd.read_excel('s1.xlsx')
 s2=pd.read_excel('s2.xlsx')
 
 s1.equals(s2)
-
+exist=0
+non_exist=0
 comparison_values = s1.values == s2.values
-
-
-rows,cols=np.where(comparison_values==False)
-
-for item in zip(rows,cols):
-    s1.iloc[item[0], item[1]] = '{} --> {}'.format(s1.iloc[item[0], 
-    item[1]],s2.iloc[item[0], item[1]])
-    if np.where(comparison_values=="False"):
-        print("Exist")
-        break
-    else :
-        print("non_exist")
-        break
+comparison_values.tolist()
+for i in range(0,len(comparison_values)):
+    for j in range(0,len(comparison_values)-1):
+        if comparison_values[i][j]==False:
+            exist=exist+1
+if exist>0:
+    print("exist")
+else:
+    print("NOn_Exist")
