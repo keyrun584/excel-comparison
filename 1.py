9ba7d3e90e9374d33638da53e7bb2cc39683c4b3
keyrun584 <89588350+keyrun584@users.noreply.github.com>
2022-10-16 16:43:35 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,17 @@
+
 import pandas as pd
 
 s1=pd.read_excel('s1.xlsx')
 s2=pd.read_excel('s2.xlsx')
-
 exist=0
 comparison_values = s1.values == s2.values
 comparison_values.tolist()
 for i in range(0,len(comparison_values)):
+    exist=0
     for j in range(0,len(comparison_values)-1):
-        if comparison_values[i][j]==False:
+        if comparison_values[i][j]==True:
             exist=exist+1
-if exist>0:
-    print("exist")
-else:
-    print("NOn_Exist")
+    if j==exist-1:
+        print("Exist")
+    else:
+        print("non_exist")
